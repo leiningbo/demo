840981be8f2b0805daac8227740b4a91b4c12aec
leiningbo <1071781349@qq.com>
2020-09-27 13:07:53 +0800

    增加 开启异步配置类，异步请使用线程池使用

---
+++ src/main/java/com/example/demo/config/AsyncConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author: leiningbo
+ * @description:  开启异步
+ * @date: Create in 13:01 2020/9/27
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+}
