768383126166c12d45cb10a74b4fbfe0e0a7d191
leiningbo <1071781349@qq.com>
2020-12-02 23:09:13 +0800

    配置jwt拦截器后 swagger2无法正常加载问题

--- src/main/java/com/example/demo/config/InterceptorConfig.java
+++ src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -4,7 +4,6 @@ import com.example.demo.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -27,20 +26,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 拦截所有请求，通过判断是否有 @UserLoginToken 注解 决定是否需要登录
         registry.addInterceptor(getTokenInterceptor()).addPathPatterns("/**")
+        // 解决spring boot 加入拦截器后swagger不能访问问题，参考：参考：https://blog.csdn.net/liu0bing/article/details/80826590?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-5.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-5.control
                 .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
     }
 
-    /**
-     * 解决spring boot 加入拦截器后swagger不能访问问题
-     * 参考：https://blog.csdn.net/liu0bing/article/details/80826590?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-5.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-5.control
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 
 }
